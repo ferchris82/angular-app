d5aff49b9cf2ad43fb885c5f44134e57f5d67a30
ChrisferDev <ferchris82@hotmail.com>
2024-09-17 11:43:30 -0500

    Versión 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports:[CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola mundo angular';
+  age = 34;
 }
